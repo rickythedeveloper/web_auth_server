7a2d2b33321488bf2a2211975aa8624690fd4491
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-09-07 15:26:02 +0100

    #2: Salt and hash passwords

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import * as core from 'express-serve-static-core';
 import { Pool, QueryResult } from 'pg';
+import bcrypt from 'bcrypt';
 
 const USER_ACCOUNTS_TABLE = 'user_accounts';
+const MAX_PASSWORD_LENGTH = 35;
 
 const pool = new Pool({
 	user: 'client',
@@ -40,20 +42,24 @@ interface NewUser {
 }
 export const createUser = (request: Request<core.ParamsDictionary, any, NewUser>, response: Response) => {
 	const { username, password } = request.body;
+	if (password.length > MAX_PASSWORD_LENGTH) throw new Error('Password is too long. This should have been handled by the client side.');
 
 	// TODO: check the username does not relady exist
-	// TODO: generate salt
-	const salt = 'salthere';
-	// TODO: generate hash
-	const passwordHash = password;
-	pool.query(
-		`INSERT INTO ${USER_ACCOUNTS_TABLE} (username, password_hash, password_salt) VALUES ($1, $2, $3)`,
-		[username, passwordHash, salt],
-		(error: Error, results: QueryResult<any>) => {
-			if (error) throw error;
-			response.status(200).send(`Added a user with username ${username}`);
-		},
-	);
+
+	bcrypt.genSalt(10, (saltError, salt) => {
+		if (saltError) throw saltError;
+		bcrypt.hash(password, salt, (hashError, hash) => {
+			if (hashError) throw hashError;
+			pool.query(
+				`INSERT INTO ${USER_ACCOUNTS_TABLE} (username, password_hash, password_salt) VALUES ($1, $2, $3)`,
+				[username, hash, salt],
+				(error: Error, results: QueryResult<any>) => {
+					if (error) throw error;
+					response.status(200).send(`Added a user with username ${username}`);
+				},
+			);
+		});
+	});
 };
 
 // export const updateUser = (request: Request<{ id: string }>, response: Response) => {
